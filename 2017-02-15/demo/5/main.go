4f774cbc402e7eab4a31437efad1862e4da2032f
Laurie Clark-Michalek <laurie@qubit.com>
2017-02-15 17:46:29 +0000

    use kn8 instead

--- 2017-02-15/demo/5/main.go
+++ 2017-02-15/demo/5/main.go
@@ -11,14 +11,9 @@ import (
 )
 
 var pushPaths = []string{
-	"/QubitProducts_exporter_exporter_files/octocat-spinner-32.gif",
-	"/QubitProducts_exporter_exporter_files/frameworks-b0d504a21e5761da5ec5eade7ed594f51a8583550c5666e515607039056c976d.js",
-	"/QubitProducts_exporter_exporter_files/github-db2eee9d5e9a883f2ba4dc3de2cace58297fe2d1f66b91292ecb6a385c12d799.js",
-	"/QubitProducts_exporter_exporter_files/github-0e373bf06af78ffa67fcc21199552cb7946a49bf88cbc2e1084257963fc45d1f.css",
-	"/QubitProducts_exporter_exporter_files/octocat-spinner-32.gif",
-	"/QubitProducts_exporter_exporter_files/frameworks-70aff62372b4dd20e8b7e3141aa52f2b7fda1b9238a597db09f7dd5bbcff25f6.css",
-	"/QubitProducts_exporter_exporter_files/78389",
-	"/QubitProducts_exporter_exporter_files/octocat-spinner-128.gif",
+	"./kn8_files/main.js",
+	"./kn8_files/commons.js",
+	"./kn8_files/css",
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
